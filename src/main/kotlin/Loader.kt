2d7ba3a75661c2af829bb86aa263412da58aba2f
BuildTools <unconfigured@null.spigotmc.org>
2020-07-19 22:03:32 +0200

    added gitignore + sample event to greet joining players

--- src/main/kotlin/Loader.kt
+++ src/main/kotlin/Loader.kt
@@ -2,12 +2,19 @@ package main
 
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandExecutor
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 public class Loader : JavaPlugin(),Listener,CommandExecutor{
     override fun onEnable() {
         super.onEnable()
-        Bukkit.broadcastMessage("Plugin loaded!")
+        Bukkit.getPluginManager().registerEvents(this,this)
+    }
+
+    @EventHandler
+    public fun joinEvent(event: PlayerJoinEvent){
+        event.player.sendMessage("Hello ${event.player.name}")
     }
 }
